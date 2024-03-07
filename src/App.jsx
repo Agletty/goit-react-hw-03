9281baaa7d4f779d42e77fc210d197838d866037
Agletty <agletty.lu@gmail.com>
2024-03-07 12:34:39 +0200

    React-HW_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,54 @@
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import { useState, useEffect } from "react";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
-import userData from "../src/components/Profile/userData.json";
-import friends from "../src/components/FriendList/friends.json";
-import transactions from "../src/components/TransactionHistory/transactions.json";
-// import "./App.css";
+import { nanoid } from "nanoid";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 function App() {
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = JSON.parse(localStorage.getItem("contacts"));
+    return (
+      storedContacts || [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ]
+    );
+  });
+
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleDelete = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  const handleAddContact = (formData) => {
+    const newContact = {
+      id: nanoid(),
+      name: formData.name,
+      number: formData.number,
+    };
+
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const searchResults = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handleAddContact} />
+      <SearchBox searchValue={searchValue} onSearchChange={setSearchValue} />
+      <ContactList contacts={searchResults} onDelete={handleDelete} />
+    </div>
   );
 }
 
